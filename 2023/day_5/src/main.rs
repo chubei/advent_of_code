82daf80eec455bf401e4bec3e8c237561b7cfc1b
chubei <914745487@qq.com>
2023-12-05 14:18:02 +0800

    better day 5

--- 2023/day_5/src/main.rs
+++ 2023/day_5/src/main.rs
@@ -14,6 +14,32 @@ impl Map {
         source
     }
 
+    fn get_range(&self, source: Range<u64>) -> Vec<Range<u64>> {
+        let mut unmapped = vec![source];
+        let mut mapped = vec![];
+        for entry in &self.entries {
+            let mut new_unmapped = vec![];
+            for source in unmapped {
+                let (entry_new_unmapped, entry_mapped) = entry.get_range(source);
+                new_unmapped.extend(entry_new_unmapped);
+                if let Some(entry_mapped) = entry_mapped {
+                    mapped.push(entry_mapped);
+                }
+            }
+            unmapped = new_unmapped;
+        }
+        mapped.extend(unmapped);
+        mapped
+    }
+
+    fn get_all_ranges(&self, sources: Vec<Range<u64>>) -> Vec<Range<u64>> {
+        sources
+            .into_iter()
+            .map(|source| self.get_range(source))
+            .flatten()
+            .collect()
+    }
+
     fn parse(input: &str) -> Map {
         let mut entries = vec![];
         for line in input.trim().lines() {
@@ -45,6 +71,51 @@ impl Entry {
         }
     }
 
+    fn get_range(&self, source: Range<u64>) -> (Vec<Range<u64>>, Option<Range<u64>>) {
+        if self.source_range().contains(&source.start)
+            && self.source_range().contains(&(source.end - 1))
+        {
+            // source is within source range
+            let destination_start = source.start - self.source_start + self.destination_start;
+            let destination_end = source.end - self.source_start + self.destination_start;
+            (vec![], Some(destination_start..destination_end))
+        } else if self.source_range().contains(&source.start)
+            && source.end > self.source_range().end
+        {
+            // source first half is within source range
+            let destination_start = source.start - self.source_start + self.destination_start;
+            let destination_end = self.destination_start + self.length;
+            (
+                vec![self.source_range().end..source.end],
+                Some(destination_start..destination_end),
+            )
+        } else if source.start < self.source_start
+            && self.source_range().contains(&(source.end - 1))
+        {
+            // source second half is within source range
+            let destination_start = self.destination_start;
+            let destination_end = source.end - self.source_start + self.destination_start;
+            (
+                vec![source.start..self.source_start],
+                Some(destination_start..destination_end),
+            )
+        } else if source.start < self.source_start && source.end > self.source_range().end {
+            // source contains source range
+            let destination_start = self.destination_start;
+            let destination_end = self.destination_start + self.length;
+            (
+                vec![
+                    source.start..self.source_start,
+                    self.source_range().end..source.end,
+                ],
+                Some(destination_start..destination_end),
+            )
+        } else {
+            // no intersection
+            (vec![source], None)
+        }
+    }
+
     // Parse "destination_start source_start length"
     fn parse(line: &str) -> Entry {
         let mut parts = line.split_whitespace();
@@ -66,7 +137,10 @@ fn main() {
     parts.next().unwrap();
     let seeds = parts.next().unwrap().lines().next().unwrap().trim();
     let seeds: Vec<u64> = seeds.split(' ').map(|s| s.parse().unwrap()).collect();
-    let seeds = seeds.chunks_exact(2);
+    let seeds = seeds
+        .chunks_exact(2)
+        .map(|chunk| chunk[0]..chunk[0] + chunk[1])
+        .collect::<Vec<_>>();
     let seed_to_soil = Map::parse(parts.next().unwrap());
     let soil_to_fertilizer = Map::parse(parts.next().unwrap());
     let fertilizer_to_water = Map::parse(parts.next().unwrap());
@@ -75,20 +149,12 @@ fn main() {
     let temperature_to_humidity = Map::parse(parts.next().unwrap());
     let humidity_to_location = Map::parse(parts.next().unwrap());
 
-    let mut result = u64::MAX;
-    for seed in seeds {
-        let start = seed[0];
-        let length = seed[1];
-        for seed in start..start + length {
-            let soil = seed_to_soil.get(seed);
-            let fertilizer = soil_to_fertilizer.get(soil);
-            let water = fertilizer_to_water.get(fertilizer);
-            let light = water_to_light.get(water);
-            let temperature = light_to_temperature.get(light);
-            let humidity = temperature_to_humidity.get(temperature);
-            let location = humidity_to_location.get(humidity);
-            result = result.min(location);
-        }
-    }
-    println!("{}", result);
+    let soils = seed_to_soil.get_all_ranges(seeds);
+    let fertilizers = soil_to_fertilizer.get_all_ranges(soils);
+    let waters = fertilizer_to_water.get_all_ranges(fertilizers);
+    let lights = water_to_light.get_all_ranges(waters);
+    let temperatures = light_to_temperature.get_all_ranges(lights);
+    let humidities = temperature_to_humidity.get_all_ranges(temperatures);
+    let locations = humidity_to_location.get_all_ranges(humidities);
+    println!("{}", locations.iter().map(|range| range.start).min().unwrap());
 }
